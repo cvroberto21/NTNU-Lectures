0addb5c0a0b44317a2ca51fa49ba4087236d9a47
Erran Hilley <hphilley15@outlook.com>
2020-01-17 16:53:58 +0800

    Added python run-jupyter module

--- Docker/run-jupyter.py
+++ Docker/run-jupyter.py
@@ -10,7 +10,7 @@ def main( argv = None ):
         argv = sys.argv[1:]
     parser = argparse.ArgumentParser( description="Start a docker image with the current directory mounted as /data")
     parser.add_argument("--data_dir", "-d", default=".")
-    parser.add_argument("container", default="abthil023/ntnu-lectures")
+    parser.add_argument("--container", default="abthil023/ntnu-lectures")
     parser.add_argument("--client_dir", "-c", default="/data")
     parser.add_argument("--port", "-p", default=8888)
     parser.add_argument("--client_port", default=8888)
@@ -18,20 +18,25 @@ def main( argv = None ):
     args = parser.parse_args( argv )
     
     data_dir = str( pathlib.Path( args.data_dir ).resolve() )
-    client_dir = str( pathlib.Path( args.client_dir ).resolve() )
+    client_dir = str( args.client_dir )
 
     cmd = [
         "docker",
         "run",
         "--volume",
-        '"' + data_dir + '"' + ":" + '"' + client_dir + '"',
+        '' + data_dir + '' + ":" + '' + client_dir + '',
         "-i",
         "-t",
         "-p",
         str(args.port) + ":" + str(args.client_port),
-        "/bin/bash --login -c",
-        f"/opt/conda/bin/conda install jupyterlab -y --quiet && /opt/conda/bin/jupyter lab --notebook-dir={args.client_dir} --ip='*' --port={args.client_port} --no-browser --NotebookApp.token='' --NotebookApp.password='111111' --allow-root"
+        '' + str( args.container ) +'',
+        "/bin/bash",
+        "--login",
+        "-c",
+       '' + f"/opt/conda/bin/conda install jupyterlab -y --quiet && /opt/conda/bin/jupyter lab --notebook-dir={args.client_dir} --ip='*' --port={args.client_port} --no-browser --NotebookApp.token='' --NotebookApp.password='' --allow-root" + ''
     ]   
+
+    print("Running cmd", " ".join(cmd) )
 
     subprocess.call( cmd )
 
