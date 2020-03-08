ec90445f3dc541b706037ea56ac9718f6631a0e6
dkclauson003 <dkclauson003@outlook.com>
2020-03-08 21:07:37 +0800

    jbdata

--- jblecture/jbdocument/__init__.py
+++ jblecture/jbdocument/__init__.py
@@ -63,7 +63,7 @@ class JBDocument:
         return current 
       
     def instTemplate( self, text, vars ):
-        print('jbdocument CFG', cfg)
+        print('jbdocument CFG', hex(id(cfg)))
         d = { ** cfg['user_ns'], **vars }
         return JBDocument.sInstTemplate( text, d )
         
